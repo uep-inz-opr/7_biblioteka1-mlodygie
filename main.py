b31da1fcadb1c6c65282c2db86b6b4c03e0ac2e9
Kuba <kubaglowacki2@gmail.com>
2022-05-25 23:07:17 +0200

    Zadanie biblioteka 1

---
+++ main.py
@@ -0,0 +1,37 @@
+class Ksiazka:
+    def __init__(self, tytul, autor, rok):
+        self.tytul = tytul
+        self.autor = autor
+        self.rok = rok
+
+ilosc_ksiazek=int(input())
+lista_ksiazek=[]
+lista_egzemplarzy=[]
+
+for i in range(ilosc_ksiazek):
+    info_ksiazka = eval(input())
+    tytul = info_ksiazka[0]
+    autor = info_ksiazka[1]
+    rok = info_ksiazka[2]
+    ksiazka = Ksiazka(tytul, autor, rok)
+    lista_ksiazek.append(ksiazka)
+
+tytul = []
+ilosc = []
+info = []
+i = 0
+
+for ksiazka in lista_ksiazek:
+    tytul.append(ksiazka.tytul)
+
+for n in tytul:
+    ilosc.append(tytul.count(n))
+
+for ksiazka in lista_ksiazek:
+    info.append(ksiazka.tytul + ksiazka.autor + str(ilosc[i]))
+    i += 1
+    info = set(info)
+    info = sorted(info)
+
+for info_egzemplarze in info:
+    print(info_egzemplarze)
